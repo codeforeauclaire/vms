8a64bb3b438e0eed3ad96fa0f1aae262e2b9b4d7
Anthony <Anthony123272@gmail.com>
2016-04-04 22:49:20 -0500

    Proof of concept local storage and reactive variable

--- client/main.js
+++ client/main.js
@@ -2,21 +2,27 @@
 /* global Meteor */
 
 import { Template } from 'meteor/templating';
+import { ReactiveVar } from 'meteor/reactive-var';
 
 import './main.html';
 
 var getStatus = function() {
 	return localStorage.getItem('status');
 };
-var setStatus = function(status) {
-	return localStorage.setItem('status', status);
+var setStatus = function(statusSetTo, reactiveVar) {
+	reactiveVar.set('spinning');
+	return localStorage.setItem('status', statusSetTo);
 };
 
 Template.main.onCreated(function() {
-	if (!getStatus()) {
-		setStatus('spinning');
+	this.status = new ReactiveVar();
+	if (getStatus()) {
+		Template.instance().status.set(getStatus());
+	} else {
+		var that = this;
+		setStatus('spinning', this.status);
 		Meteor.call('spinUpNewVM', function() {
-			setStatus('spun');
+			setStatus('spun', that.status);
 		});
 	}
 });
@@ -24,11 +30,14 @@ Template.main.onCreated(function() {
 Template.main.helpers({
   counter() {
     return Template.instance().counter.get();
+  },
+  status() {
+    return Template.instance().status.get();
   }
 });
 
 Template.main.events({
   'click button'(event, instance) {
-		console.log(getStatus());
+	console.log(Template.instance().status.get());
   }
 });
