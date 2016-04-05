3097f69346087a9a303a31f16c379c4e39a988ac
Anthony <Anthony123272@gmail.com>
2016-04-05 16:02:03 -0500

    Self destruct old servers automated

--- server/main.js
+++ server/main.js
@@ -1,17 +1,52 @@
 'use strict';
-/*globals Meteor*/
+/*globals Meteor, _, moment*/
 
 import { Meteor } from 'meteor/meteor';
 import DigitalOceanApi from 'digital-ocean-api';
 import Future from 'fibers/future';
 
+function selfDestructOldServers() {
+	console.log('selfDestructOldServers(...)');
+	var api = new DigitalOceanApi({
+		token: Meteor.settings.digitalocean.apitoken
+	});
+	api.listDroplets(function(err, droplets) {
+		if (err) {
+			throw err;
+		}
+		_.each(droplets, function(droplet) {
+			var key = 'created_at';
+			var serverShutdownAfter = moment(droplet[key])
+				.add(Meteor.settings.public.serverlifespanhours, 'hours')
+				.subtract(20, 'minutes');
+			var pastShutdownAfter = (moment().diff(serverShutdownAfter) > 0);
+
+			// Delete old servers with our name
+			if (pastShutdownAfter && (droplet.name === 'vms-virtual-machine')) {
+				api.deleteDroplet(droplet.id, function(err, data) {
+					if (err) {
+						console.log('Problem shutting down droplet #' + droplet.id);
+						console.log(err);
+					} else {
+						console.log('Shut down droplet #' + droplet.id);
+					}
+				});
+			}
+		});
+	});
+}
+
 Meteor.startup(() => {
-  // code to run on server at startup
+	selfDestructOldServers();
+
+	// Self destruct old servers every 10 minutes
+	setInterval(selfDestructOldServers, 10 * 60 * 1000);
 });
 
 Meteor.methods({
 	'spinUpNewVM': function() {
 		console.log('spinUpNewVM');
+		// TODO: Abstract API setup
 		var api = new DigitalOceanApi({
 			token: Meteor.settings.digitalocean.apitoken
 		});
