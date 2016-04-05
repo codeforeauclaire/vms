bb5aa9373b170edba642683522e5935ec99a936e
Anthony <Anthony123272@gmail.com>
2016-04-04 21:58:36 -0500

    Starting boilerplate, more todo's

--- README.md
+++ README.md
@@ -5,3 +5,6 @@
 1. Setup to accept Anthony's started settings to deploy to his mets
 1. Setup Jobs to destroy servers after it's been up for 1 hour
 1. Think through / review more / add more here as needed
+1. Add throttling to prevent people from adding too many
+ 1. At the client level?
+1. Add config for how long server stays up (6 hours I think)
--- client/main.js
+++ client/main.js
@@ -3,20 +3,18 @@ import { ReactiveVar } from 'meteor/reactive-var';
 
 import './main.html';
 
-Template.hello.onCreated(function helloOnCreated() {
-  // counter starts at 0
-  this.counter = new ReactiveVar(0);
+Template.main.onCreated(function() {
+	this.counter = new ReactiveVar(0);
 });
 
-Template.hello.helpers({
+Template.main.helpers({
   counter() {
     return Template.instance().counter.get();
   },
 });
 
-Template.hello.events({
+Template.main.events({
   'click button'(event, instance) {
-    // increment the counter when button is clicked
-    instance.counter.set(instance.counter.get() + 1);
+	  console.log('clicked');
   },
 });
