cf261723976440ff80b13e6595d54850a45e9d8a
Anthony <Anthony123272@gmail.com>
2016-04-05 07:52:36 -0500

    Proof of concept spin up a digital ocean server

--- server/main.js
+++ server/main.js
@@ -1,5 +1,6 @@
 'use strict';
 import { Meteor } from 'meteor/meteor';
+import DigitalOceanApi from 'digital-ocean-api';
 
 Meteor.startup(() => {
   // code to run on server at startup
@@ -9,6 +10,26 @@ Meteor.methods({
 	'spinUpNewVM': function() {
 		console.log('spin it');
 		console.log(Meteor.settings.digitalocean.apitoken);
+		var api = new DigitalOceanApi({
+			token: Meteor.settings.digitalocean.apitoken
+		});
 		Meteor._sleepForMs(2000);
+		var requestBody = {
+			name: 'test',
+			region: 'nyc3',
+			size: '512mb',
+			image: 'ubuntu-14-04-x64',
+			'ssh_keys': null,
+			backups: false,
+			ipv6: false,
+			'user_data': null,
+			'private_networking': null
+		};
+		api.createDroplet(requestBody, function(err, data) {
+			if (err) {
+				return false;
+			}
+			return data;
+		});
 	}
 });
