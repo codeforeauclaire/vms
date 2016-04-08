3a1293386c97b64881c038965c6573dd0971b885
Anthony Astige <AnthonyAstige@users.noreply.github.com>
2016-04-08 12:18:00 -0500

    Update vmsquickinstall.sh

--- bin/vmsquickinstall.sh
+++ bin/vmsquickinstall.sh
@@ -5,7 +5,8 @@
 # Update all software & install new
 sudo apt-get update && sudo apt-get upgrade -y && sudo apt-get install -y git
 
-# Setup swap (will enable on reboot)
+# Setup swap (which will enable on reboot)
+# * We need more than 512MB for Meteor to work. Warning: it may be slow on the small vms instances
 # * https://www.digitalocean.com/community/tutorials/how-to-add-swap-on-ubuntu-14-04
 sudo fallocate -l 4G /swapfile
 sudo chmod 600 /swapfile
@@ -13,7 +14,7 @@ sudo mkswap /swapfile
 echo "/swapfile   none    swap    sw    0   0" >> "/etc/fstab"
 echo "vm.swappiness=10" >> /etc/sysctl.conf
 echo "vm.vfs_cache_pressure = 50" >> /etc/sysctl.conf
-# * Enable swap file now
+# * Enable swap file now before reboot
 sudo swapon /swapfile
 
 # Install meteor
