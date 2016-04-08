c653521d2c3e3fca9b62ab6bdeee5c6cf2f8f40e
Anthony Astige <AnthonyAstige@users.noreply.github.com>
2016-04-08 12:09:35 -0500

    Update vmsquickinstall.sh

--- bin/vmsquickinstall.sh
+++ bin/vmsquickinstall.sh
@@ -3,7 +3,7 @@
 { # this ensures the entire script is downloaded and run #
 
 # Update all software & install new
-sudo apt-get update && sudo apt-get upgrade -y && sudo apt-get install -y
+sudo apt-get update && sudo apt-get upgrade -y && sudo apt-get install -y git
 
 # Install meteor
 curl https://install.meteor.com/ | sh
