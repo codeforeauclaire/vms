1561ead41b3bb13442e5aef3518c4e36127bb67f
Anthony Astige <AnthonyAstige@users.noreply.github.com>
2016-04-08 12:05:49 -0500

    Update vmsquickinstall.sh
    First draft quick vms install script

---
+++ bin/vmsquickinstall.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+{ # this ensures the entire script is downloaded and run #
+
+# Update all software & install new
+sudo apt-get update && sudo apt-get upgrade -y && sudo apt-get install -y
+
+# Install meteor
+curl https://install.meteor.com/ | sh
+
+# Clone repository
+git clone https://github.com/codeforeauclaire/vms.git /root/vms
+
+} # this ensures the entire script is downloaded and run #
